e0f313886a67993bc0cdaceecbee10a738f11c02
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-06-05 12:52:08 +0530

    hardhat fix

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,11 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
 
-dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const RPC_URL = process.env.RPC_URL || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const RPC_URL = process.env.RPC_URL || "";
 
 // const graphitetestnet = {
 //   id: 54170,
@@ -20,7 +18,7 @@ const config: HardhatUserConfig = {
   networks: {
     graphite: {
       url: RPC_URL,
-      accounts: ["ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      accounts: [""],
     },
   },
   paths: {
